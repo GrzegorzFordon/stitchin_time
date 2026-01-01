354b1af5a5a10ff0063ae2804765fdae0e4d5f8e
GrzegorzFordon <Grzegorz.fordon@gmail.com>
2026-01-01 16:23:31 +0100

    First Commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "13304840"
+    serviceWorkerVersion: "2468150663"
   }
 });
